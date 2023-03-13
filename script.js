f9494397a4aa4f6ea8a481b438b6d66d5513f5f7
jan0803 <126251323+jan0803@users.noreply.github.com>
2023-03-13 11:25:33 +0100

    Change Whitespace

--- script.js
+++ script.js
@@ -29,14 +29,12 @@ function initilizeGrid() {
 function addSquaresToHTML () {
     //inserting the squares into the container, depending on the gridSize
     for (let i=0; i < gridSize; i++) {
-
         for (let j=0; j < gridSize; j++) {
             let square = document.createElement('div');
             square.className = 'square';
 
             sketchContainer.appendChild(square);
         }
-
     }
 }
 
